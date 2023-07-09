7aed7a6919eb1dca81c25c8eeec9d9ba5dffc184
Pablo Canseco <p@blocanse.co>
2023-07-08 19:49:43 -0600

    upload current state of the main apps, they work as-is

--- cmd/getplan/main.go
+++ cmd/getplan/main.go
@@ -18,19 +18,22 @@ func main() {
 	tinyTime.SetTime(time.Now())
 
 	sat := satellite.NewSatellite(
-		"1 25544U 98067A   23169.18024747  .00013351  00000-0  24027-3 0  9995",
-		"2 25544  51.6405 323.6092 0004372  52.6861 119.0308 15.50144016401949",
+		"1 25544U 98067A   23185.67588067  .00125576  00000-0  21210-2 0  9990",
+		"2 25544  51.6410 241.8291 0004659  96.9441  45.8138 15.50681602404509",
 		gosat.GravityWGS84)
 
 	passes := sat.Plan(satellite.Coordinates{
 		LatitudeDegrees:  cfg.HomeLatitudeDeg,
 		LongitudeDegrees: cfg.HomeLongitudeDeg,
 		AltitudeKM:       cfg.HomeAltitudeKM,
-	}, 10, 45, time.Now(), time.Now().Add(2*24*time.Hour), time.Second*2)
+	}, 20, 45, time.Now(), time.Now().Add(2*24*time.Hour), time.Second*1)
 
 	plan := satellite.Plan{
 		Passes: passes,
 	}
+
+	fakePass := plan.Passes[0].CopyPassStartingAt(tinyTime.GetTime().Add(10*time.Second), 1*time.Second)
+	plan.Passes = []satellite.Pass{fakePass}
 
 	planJSON, err := json.Marshal(plan)
 	if err != nil {
--- cmd/motors/main.go
+++ cmd/motors/main.go
@@ -4,39 +4,124 @@ import (
 	"machine"
 	"time"
 
+	"github.com/pmcanseco/go-sat-tracker/internal/motors"
 	"github.com/pmcanseco/go-sat-tracker/internal/steppermotor"
+	"github.com/pmcanseco/go-sat-tracker/internal/tracking"
+)
+
+// wire the motor cable from the top down: (right side of driver, pot on the bottom left)
+// - blue
+// - red
+// - green
+// - black
+// where blue is immediately adjacent to gnd on the top
+// and there's a one-pin gap between the gnd on the bottom
+
+const (
+	ElevationSleepPin     = machine.GPIO8  // WIRING - Pin 6 white (immediately below pins 10 and 9)
+	ElevationStepPin      = machine.GPIO7  // WIRING - Pin 5 green
+	ElevationDirectionPin = machine.GPIO10 // WIRING - blue
+	AzimuthSleepPin       = machine.GPIO26 // WIRING - A0 white
+	AzimuthStepPin        = machine.GPIO27 // WIRING - A1 green
+	AzimuthDirectionPin   = machine.GPIO28 // WIRING - A2 blue
 )
 
 func main() {
-	dir := machine.GPIO10
+	//dir := machine.GPIO10
+	//dir.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	//dir.High()
+	//
+	//step := machine.GPIO9
+	//step.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	//step.Low()
+	//
+	//sleep := machine.GPIO8
+	//sleep.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	//sleep.Low()
+	//
+	//sm := steppermotor.New(steppermotor.DeviceConfig{
+	//	StepPin:   step,
+	//	DirPin:    dir,
+	//	SleepPin:  sleep,
+	//	StepCount: 200,
+	//	RPM:       40,
+	//})
+	//
+	//for {
+	//	sm.Enable()
+	//	sm.Move(200)
+	//	sm.Disable()
+	//	time.Sleep(5 * time.Second)
+	//
+	//	sm.Enable()
+	//	sm.Move(-200)
+	//	sm.Disable()
+	//	time.Sleep(5 * time.Second)
+	//}
+
+	elm := getElevationMotor()
+	azm := getAzimuthMotor()
+
+	for {
+		azm.CommandAngle(340)
+		elm.CommandAngle(90)
+		time.Sleep(15 * time.Second)
+		azm.CommandAngle(45)
+		elm.CommandAngle(45)
+		time.Sleep(15 * time.Second)
+		azm.CommandAngle(270)
+		elm.CommandAngle(15)
+		time.Sleep(15 * time.Second)
+		azm.CommandAngle(0)
+		time.Sleep(15 * time.Second)
+	}
+}
+
+func getElevationMotor() tracking.Angler {
+	dir := ElevationDirectionPin
 	dir.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	dir.High()
 
-	step := machine.GPIO9
+	step := ElevationStepPin
 	step.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	step.Low()
 
-	sleep := machine.GPIO8
+	sleep := ElevationSleepPin
 	sleep.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	sleep.Low()
 
 	sm := steppermotor.New(steppermotor.DeviceConfig{
-		StepPin:   step,
-		DirPin:    dir,
-		SleepPin:  sleep,
-		StepCount: 200,
-		RPM:       50,
-	})
+		StepPin:        step,
+		DirPin:         dir,
+		SleepPin:       sleep,
+		EnableSleeping: false,
+		StepCount:      200,
+		RPM:            40,
+	}, false)
 
-	for {
-		sm.Enable()
-		sm.Move(200)
-		sm.Disable()
-		time.Sleep(10 * time.Second)
+	return motors.New(sm, 15)
+}
 
-		sm.Enable()
-		sm.Move(-200)
-		sm.Disable()
-		time.Sleep(10 * time.Second)
-	}
+func getAzimuthMotor() tracking.Angler {
+	dir := AzimuthDirectionPin
+	dir.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	dir.High()
+
+	step := AzimuthStepPin
+	step.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	step.Low()
+
+	sleep := AzimuthSleepPin
+	sleep.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	sleep.Low()
+
+	sm := steppermotor.New(steppermotor.DeviceConfig{
+		StepPin:        step,
+		DirPin:         dir,
+		SleepPin:       sleep,
+		EnableSleeping: false,
+		StepCount:      200,
+		RPM:            60,
+	}, false)
+	return motors.New(sm, 0)
 }
